3bdb49767bc82cc163f162d23c5a9041617c96cc
drinkgalaxy <drinkgalaxy1226@gmail.com>
2025-05-03 01:20:22 +0900

    Init: 프로젝트 생성

---
+++ src/test/java/com/example/webSocketGame/WebSocketGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webSocketGame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSocketGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
